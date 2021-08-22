5e88b3057d9f081aafaad29d6bcc39b6ee686150
Javiernova <89332717+Javiernova@users.noreply.github.com>
2021-08-22 03:42:31 -0500

    repositorio
    Repositorio trabajo colaborativo con tres
    puntos asignados

---
+++ Taller colaborativo/src/co/edu/uptc/logic/Management.java
@@ -0,0 +1,64 @@
+package co.edu.uptc.logic;
+
+/**
+ * 
+ * @author USUARIO
+ *
+ */
+
+public class Management {
+	
+	private static String CHAR_STRING;
+
+	
+	/**
+	 * Metodo el cual muestra los caracteres que se repiten en la cadena de caracteres
+	 * @param sentenc
+	 * @return string
+	 */
+	public static String encrypt (String sentenc) {
+
+		char array[] = sentenc.toCharArray();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (char) (array[i] + (char) 8);
+		}
+		String encrypted = String.valueOf(array);
+		return encrypted;
+	}
+	
+	/**
+	 * metodo el cual desencripta cadena del metodo anterior
+	 * @param sentenc
+	 * @return retorna cadena desncriptada del metodo anterior
+	 */
+	
+	public static String decrypt(String sentenc) {
+		char array[] = sentenc.toCharArray();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (char) (array[i] - (char) 8);
+		}
+		String encrypted = String.valueOf(array);
+		return encrypted;
+	}
+	
+	/**
+	 * metodo que muestra la cadena ingresada sin caracteres repetidos
+	 * @param sentence 
+	 * @return string sin caracteres repetidos del parametro ingresado.
+	 */
+	
+	public String intersection(String sentence) {
+		String duple =sentence; 
+		String duplicate = "";
+		for (int i = 0; i < sentence.length(); i++) {
+			String ct = Character.toString(sentence.charAt(i));
+			if (duple.contains(ct)) {
+				if (!duplicate.contains(ct)) {
+					duplicate += ct;
+				}
+			}
+		}
+		return duplicate;
+	}
+	
+}
---
+++ Taller colaborativo/src/co/edu/uptc/runner/Runner.java
@@ -0,0 +1,75 @@
+package co.edu.uptc.runner;
+
+import javax.swing.JOptionPane;
+
+import co.edu.uptc.logic.Management;
+
+public class Runner {
+	
+	public static void menu() {
+		String sentence="";
+		
+		Management lo = new Management();
+		int menu=0;
+		do {
+			
+			try {
+				
+				
+			menu = Integer.parseInt(JOptionPane.showInputDialog("1. Convertir en nombre propio el contenido de la cadena\n"
+							+ "2.Buscar palabra\n3.Encriptar(método estático)\n4.Desencriptar (método estático)\n5.Llenar cacarter\n6.Borrar cracteres\n"
+							+ "7.Intersección\n8.Diferencia\n9.Borrar caracteres iziquierda o derecha\n10.Convertir a fecha\n11.Salir"));
+			switch (menu) {
+			case 1:
+				
+				break;
+			case 2:
+					
+				break;
+			case 3:
+				sentence=JOptionPane.showInputDialog("escriba palabra u oracion a encriptar");
+				sentence=lo.encrypt(sentence);
+				JOptionPane.showMessageDialog(null, sentence);
+				break;
+			case 4:
+				JOptionPane.showMessageDialog(null, lo.decrypt(sentence));
+				break;
+			case 5:
+				
+				break;
+			case 6:
+			
+			
+				break;
+			case 7:
+				sentence=JOptionPane.showInputDialog("escriba palabra a intersectar");
+				JOptionPane.showMessageDialog(null, lo.intersection(sentence));
+				break;
+			case 8:
+				
+				break;
+			case 9:
+				
+				break;
+			case 10:
+				
+				break;
+			case 11:
+				JOptionPane.showMessageDialog(null, "Ha salido del programa","ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+				
+				break;
+				default: JOptionPane.showMessageDialog(null, "Opcion invalida","ERROR",JOptionPane.ERROR_MESSAGE);
+			}
+			}catch(Exception e) {
+				JOptionPane.showMessageDialog(null, "Error");
+			}
+		}while(menu!=11);
+	}
+
+	public static void main(String[] args) {
+	menu();
+	
+
+	}
+
+}
