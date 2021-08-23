36951318f52d765508e65ff70121eb65546e92cc
LauraTamayo12 <laura.tamayo01@uptc.edu.co>
2021-08-23 13:45:07 -0500

    Version L

--- Taller_Colaborativo/src/co/edu/uptc/logic/Management.java
+++ Taller_Colaborativo/src/co/edu/uptc/logic/Management.java
@@ -1,13 +1,69 @@
 package co.edu.uptc.logic;
 
 public class Management {
-	
-   private static String CHAR_STRING;
-     
-    public Management(String char_string) {
-    	
-    	CHAR_STRING=char_string;
-    }
+	/*
+        + Cadena a usar inmutable
+        */
+	private static String CHAR_STRING;
+
+	/*
+     + @param constructor de la clase
+     */
+	public Management(String CHAR_STRING) {
+		this.CHAR_STRING = CHAR_STRING;
+	}
+
+	/*
+     + Método que consiste en convertir una frase es un nombre propio.
+     + @return cadena como un nombre propio
+     */
+	public String own_Name(){
+		String phrase = "";
+		char first_letter = ' ';
+		char characters = ' ';
+		int aux = 0;
+
+		for (int i = 0; i < CHAR_STRING.length(); i++) {
+			first_letter = CHAR_STRING.charAt(0);
+			characters = CHAR_STRING.charAt(i);
+			if (first_letter == ' '){
+				phrase = "No se puede iniciar con un espacio el nombre propio";
+			}else if(!Character.isLetter(first_letter)){
+				phrase = "No se puede iniciar con un caracter \n"
+						+ "diferente al alfabeto" + first_letter;
+			}
+			if (i == 0){
+				characters = Character.toUpperCase(characters);
+			}
+			if (i >= 1){
+				characters = Character.toLowerCase(characters);
+			}
+			if (characters == ' '){
+				aux = (i+1);
+			}
+			if (aux == i){
+				characters = Character.toUpperCase(characters);
+			}
+
+			phrase += characters;
+		}
+		return phrase;
+	}
+
+	/*
+     + Método que busca una palabra dada y la cantidad de veces que esta repite.
+     + @return cantidad de veces encontrada la palabra en la cadena - aux
+     */
+	public int findCharacterString(String word){
+		int aux = 0;
+		String[] chars = CHAR_STRING.split("\\W+");
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i].equalsIgnoreCase(word)){
+				aux = aux+1;
+			}
+		}
+		return aux;
+	}
 	
 	/**
 	 * Metodo el cual muestra los caracteres encriptados de la cadena de caracteres
@@ -39,13 +95,49 @@ public class Management {
 		String encrypted = String.valueOf(array);
 		return encrypted;
 	}
-	
+
+
+    /*
+    + Método para agregar un caracter segun la cantidad ed veces dada.
+    + Ya sea por izquierda o por derecha
+    + @param character
+    + @param quantify
+    + @param address
+    + @return auxCharString
+     */
+
+	public String fill_Characters(char character, int quantify, String address){
+		String modified_String = "";
+		String auxCharString = CHAR_STRING;
+		for (int i = 0; i < quantify; i++) {
+			if (address.equals("Derecha")){
+				modified_String = auxCharString + character;
+			}else{
+				modified_String = character + auxCharString;
+			}
+			auxCharString = modified_String;
+		}
+		return auxCharString;
+	}
+
+    /*
+    + Metodo que elimina el caracter otorgado en el runner, en toda la cadena
+    + @param del_char
+    + @return result
+     */
+
+	public String delete_Characters(char del_char){
+		String character = String.valueOf(del_char);
+		String result = CHAR_STRING.replace(character,"");
+		return result;
+	}
+
 	/**
 	 * metodo que muestra la cadena ingresada sin caracteres repetidos
 	 * @param sentence 
 	 * @return string sin caracteres repetidos del parametro ingresado.
 	 */
-	
+
 	public String intersection(String sentence) {
 		 
 		String duplicate = "";
@@ -59,6 +151,4 @@ public class Management {
 		}
 		return duplicate;
 	}
-	
-
 }
--- Taller_Colaborativo/src/co/edu/uptc/runner/Runner.java
+++ Taller_Colaborativo/src/co/edu/uptc/runner/Runner.java
@@ -8,58 +8,136 @@ public class Runner {
 	
 	public static void menu() {
 		String sentence="";
-		String text=JOptionPane.showInputDialog("Digite cadena inmutable: ");
-		Management mg = new Management(text);
-		int menu=0;
-		do {
-			
-			try {
-				
-				
-			menu = Integer.parseInt(JOptionPane.showInputDialog("1. Convertir en nombre propio el contenido de la cadena\n"
-							+ "2.Buscar palabra\n3.Encriptar(m�todo est�tico)\n4.Desencriptar (m�todo est�tico)\n5.Llenar cacarter\n6.Borrar cracteres\n"
-							+ "7.Intersecci�n\n8.Diferencia\n9.Borrar caracteres iziquierda o derecha\n10.Convertir a fecha\n11.Salir"));
-			switch (menu) {
-			case 1:
-				
-				break;
-			case 2:
-					
-				break;
-			case 3:
-				sentence=JOptionPane.showInputDialog("escriba palabra u oracion a encriptar");
-				sentence=mg.encrypt(sentence);
-				JOptionPane.showMessageDialog(null, sentence);
-				break;
-			case 4:
-				JOptionPane.showMessageDialog(null, mg.decrypt(sentence));
-				break;
-			case 5:
-				
-				break;
-			case 6:
-			
-			
-				break;
-			case 7:
-				sentence=JOptionPane.showInputDialog("escriba palabra a intersectar");
-				JOptionPane.showMessageDialog(null, mg.intersection(sentence));
-				break;
-			case 8:
-				
-				break;
-			case 9:
-				
-				break;
-			case 10:
-				
-				break;
-			case 11:
-				JOptionPane.showMessageDialog(null, "Ha salido del programa","ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
-				
-				break;
-				default: JOptionPane.showMessageDialog(null, "Opcion invalida","ERROR",JOptionPane.ERROR_MESSAGE);
+		boolean aux = true;
+		while(aux == true){
+			sentence = JOptionPane.showInputDialog("Ingrese la frase con la que desea utilizar el programa");
+			if (sentence == null){
+				JOptionPane.showMessageDialog(null, "Ha ocurrido un error, por lo tanto se cerrara la aplicacion", "ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+				System.exit(0);
 			}
+			if (sentence.equals("")){
+				JOptionPane.showMessageDialog(null, "Asegurese de haber ingresado una frase o cadena de texto", "ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+			}else{
+				aux = false;
+			}
+		}
+		Management mg = new Management(sentence);
+		int menu = 0;
+		do {
+			try {
+				menu = Integer.parseInt(JOptionPane.showInputDialog("1. Convertir en nombre propio el contenido de la cadena \n"
+						+ "2. Buscar palabra \n 3. Encriptar(método estático) \n 4. Desencriptar (método estático) \n 5. Llenar caracteres \n6. Borrar caracteres \n"
+						+ "7. Intersección \n 8. Diferencia \n 9. Borrar caracteres iziquierda o derecha \n 10. Validar dirección de correo electronico \n 11. Salir"));
+				switch (menu) {
+					case 1:
+						JOptionPane.showMessageDialog(null, "Sentencia : " + sentence + "\n Sentencia modificada: " + mg.own_Name());
+						break;
+					case 2:
+						boolean auxP2 = false;
+						while (auxP2 == false){
+							String word = JOptionPane.showInputDialog("Ingrese la palabra que desea buscar en la cadena de texto");
+							if (word != null){
+								if (word.equals("")){
+									JOptionPane.showMessageDialog(null, "Opcion invalida, debe escribir algun texto","ERROR",JOptionPane.ERROR_MESSAGE);
+								}else{
+									JOptionPane.showMessageDialog(null, word + " Se encuentra: " + mg.findCharacterString(word) + " veces");
+									auxP2 = true;
+								}
+							}else{
+								JOptionPane.showMessageDialog(null, "Volviendo al menú", "ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+								auxP2 = true;
+							}
+						}
+						break;
+					case 3:
+						sentence=JOptionPane.showInputDialog("escriba palabra u oracion a encriptar");
+						sentence=mg.encrypt(sentence);
+						JOptionPane.showMessageDialog(null, sentence);
+						break;
+					case 4:
+						JOptionPane.showMessageDialog(null, mg.decrypt(sentence));
+						break;
+					case 5:
+						boolean menu2 = false;
+						while(menu2 == false){
+							try {
+								int options2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese: \n 1. Para llenar caracteres por derecha\n"
+										+ "2. Para llenar caracteres por izquerda"));
+								String stringOptions2 = String.valueOf(options2);
+								if (stringOptions2 != null){
+									if (stringOptions2.equals("")){
+										JOptionPane.showMessageDialog(null, "Opcion invalida, debe elegir alguna opcion","ERROR",JOptionPane.ERROR_MESSAGE);
+									}else {
+										switch (options2){
+											case 1:
+												String address = "Derecha";
+												String character = JOptionPane.showInputDialog("Ingrese el caracter que se agregara a la sentencia indicada al inicio");
+												char character2 = character.charAt(0);
+												int limit = Integer.parseInt(JOptionPane.showInputDialog("Ingrese (en número) de la cantidad de caracteres a añadir"));
+												JOptionPane.showMessageDialog(null, mg.fill_Characters(character2, limit,address));
+												menu2 = true;
+												break;
+
+											case 2:
+												String addressLeft = "Izquierda";
+												String characterLeft = JOptionPane.showInputDialog("Ingrese el caracter que se agregara a la sentencia indicada al inicio");
+												char character2Left = characterLeft.charAt(0);
+												int limitLeft = Integer.parseInt(JOptionPane.showInputDialog("Ingrese (en número) de la cantidad de caracteres a añadir"));
+												JOptionPane.showMessageDialog(null, mg.fill_Characters(character2Left, limitLeft,addressLeft));
+												menu2 = true;
+												break;
+										}
+									}
+								}else{
+									JOptionPane.showMessageDialog(null, "Volviendo al menú", "ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+									menu2 = true;
+								}
+							}catch (Exception e){
+								JOptionPane.showMessageDialog(null, "Debe ingresar algun dato");
+							}
+						}
+						break;
+					case 6:
+						boolean continueP6 = true;
+						while (continueP6 == true){
+							try {
+								String del_Char = JOptionPane.showInputDialog("Ingrese el caracter que desea eliminar");
+								if (del_Char != null) {
+									if (del_Char.equals("")) {
+										JOptionPane.showMessageDialog(null, "Opcion invalida, debe escribir algun caracter valido", "ERROR", JOptionPane.ERROR_MESSAGE);
+									} else {
+										char del_char = del_Char.charAt(0);
+										JOptionPane.showMessageDialog(null, mg.delete_Characters(del_char));
+										continueP6 = false;
+									}
+								}else{
+									JOptionPane.showMessageDialog(null, "Volviendo al menú", "ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+									continueP6 = false;
+								}
+							}catch (Exception e){
+								JOptionPane.showMessageDialog(null, "Debe ingresar algun dato");
+							}
+						}
+						break;
+					case 7:
+						sentence=JOptionPane.showInputDialog("escriba palabra a intersectar");
+						JOptionPane.showMessageDialog(null, mg.intersection(sentence));
+						break;
+					case 8:
+
+						break;
+					case 9:
+
+						break;
+					case 10:
+
+						break;
+					case 11:
+						JOptionPane.showMessageDialog(null, "Ha salido del programa","ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+
+						break;
+					default: JOptionPane.showMessageDialog(null, "Opcion invalida","ERROR",JOptionPane.ERROR_MESSAGE);
+				}
 			}catch(Exception e) {
 				JOptionPane.showMessageDialog(null, "Error");
 			}
